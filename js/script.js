689b3707b90aba66f7cd809c6d643931c0552298
lorenzo-damico <loredam@hotmail.it>
2020-08-06 15:16:16 +0200

    aggiunto saluto con colore diverso

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+var nomeUtente = prompt("Come ti chiami?");
 
+var sesso = prompt("Qual è il tuo sesso?");
+
+var nome = document.getElementById("nome");
+
+if (!isNaN(nomeUtente) || !isNaN(sesso)) {
+  alert("Non sono ammessi valori numerici");
+
+} else if (sesso != "maschio" && sesso != "femmina") {
+  alert("Sono ammessi solo i valori maschio o femmina")
+
+} else {
+  nome.innerHTML = nomeUtente;
+
+  if (sesso == "maschio") {
+    nome.classList.add("text_blue");
+
+  } else {
+    nome.classList.add("text_pink");
+  }
+}
