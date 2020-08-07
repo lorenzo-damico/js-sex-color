eeb0bb766ed5af3bda98b02e36433c6df7420d8f
lorenzo-damico <loredam@hotmail.it>
2020-08-07 11:07:51 +0200

    Modifica

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 var nomeUtente = prompt("Come ti chiami?");
 
-var sesso = prompt("Qual è il tuo sesso?");
+var sesso = prompt("Inserisci il tuo sesso (M o F)?");
 
 var nome = document.getElementById("nome");
 
 if (!isNaN(nomeUtente) || !isNaN(sesso)) {
   alert("Non sono ammessi valori numerici");
 
-} else if (sesso != "maschio" && sesso != "femmina") {
-  alert("Sono ammessi solo i valori maschio o femmina")
+} else if (sesso != "M" && sesso != "F") {
+  alert("Sono ammessi solo i valori M o F")
 
 } else {
   nome.innerHTML = nomeUtente;
 
-  if (sesso == "maschio") {
+  if (sesso == "M") {
     nome.classList.add("text_blue");
 
   } else {
